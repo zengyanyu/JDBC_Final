b82b52f017b73c2ef6b4894ff2929f7873ce55bf
1194314874@qq.com <1194314874@qq.com>
2026-05-19 20:29:13 +0800

    添加JDBC对象

--- src/com/zengyy/test/IStudentDAOTest.java
+++ src/com/zengyy/test/IStudentDAOTest.java
@@ -1,22 +1,18 @@
 package com.zengyy.test;
 
 import com.zengyy.dao.IStudentDAO;
-import com.zengyy.dao.impl.IStudentDAOImpl;
+import com.zengyy.dao.impl.StudentDAOImpl;
 import com.zengyy.domain.Student;
 import org.junit.Test;
 
-import javax.lang.model.SourceVersion;
-
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 /**
  * @author zengyanyu
  */
 public class IStudentDAOTest {
 
-    private IStudentDAO dao = new IStudentDAOImpl();
+    private IStudentDAO dao = new StudentDAOImpl();
 
     @Test
     public void save() {
